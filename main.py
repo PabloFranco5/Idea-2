74a4479fbc9a787760f72cc20edb17947a69b687
Juan Pablo Montoya <juanpismontollaxd@gmail.com>
2022-09-11 10:06:14 -0500

    funciones comentadas

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from datetime import date,datetime
+from mailbox import NoSuchMailboxError
 from pydoc import doc
+import numpy as np
+import pandas as pd
 
 class infopersonalpaciente:
     def __init__(self, tipo_doc,documento, nombre, apellido, numero_contacto, ciudad_residencia, estado_civil, nacionalidad, numero_emergencia, ultima_actualizacion):
@@ -12,12 +15,29 @@ class infopersonalpaciente:
         self.estado_civil=estado_civil
         self.nacionalidad= nacionalidad
         self.numero_emergencia=numero_emergencia
-        self.ultima_actulizacion= (ultima_actualizacion)
-    def __str__(self):
-        return(f"Paciente {self.nombre} {self.apellido}, identificado con {self.tipo_doc} {self.documento} con número de contacto {self.numero_contacto}, reside en {self.ciudad_residencia}")
+        self.ultima_actulizacion= ultima_actualizacion
+    
+    def paiente(self):
+        """
+        Esta funcion tinene el proposito de dar un resumen informativo acerca de los datos proporcionados por el paciente en cuestion al que 
+        pertenecen los datos mediante un return 
+
+        """
+        #dict={"tipo_doc":self.tipo_doc, "documento": self.documento,"nombre" :self.nombre, "apellido":self.apellido,"numero_contacto": self.numero_contacto,"ciudad_residencia": self.ciudad_residencia, "estado_civil":self.estado_civil, "nacionalidad":self.nacionalidad, "numero_emergencia": self.numero_emergencia,"ultima_actualizacion":self.ultima_actulizacion}
+        #df=pd.DataFrame(dict)
+        #return(df)
         
+        return(f"El paciente {self.nombre} {self.apellido}, identificado con {self.tipo_doc} {self.documento} con nacionalidad {self.nacionalidad} reside en {self.ciudad_residencia}, se encuentra {self.estado_civil} y cuenta con un el numero {self.numero_contacto} para contactarlo y para cualquier emergencia se cuenta con el numero {self.numero_emergencia}")
+
+class infopaciente:
+    def __init__(self,tipo_doc,documento,apellido,nombre):
+        infopersonalpaciente.__init__(tipo_doc,documento, nombre, apellido)
+    def paciente(self):
+        pass
+        #return(f"El paciente {self.nombre} {self.apellido}, identificado con {self.tipo_doc} {self.documento}")
 
 class infomedica:
+
     def __init__(self, tipo_doc,documento, historia, edad, sexo, RH, sanguineo, peso, estatura, reanimacion, alergias, comorbilidades, embarazos, FUM, observaciones, remision, especialista, medicacion, actualizacion):
         self.tipo_doc=tipo_doc
         self.documento=documento
@@ -38,15 +58,26 @@ class infomedica:
         self.especialista=especialista
         self.medicacion=medicacion
         self.actualizacion=actualizacion
-    def __str__(self) -> str:
-        pass
+    def medica(self) -> str:
+        """
+        esta funcion cumple con el proposito de dar un descripcion detallada de la informacion 
+        medica del paciente proporcionada por el mismo con la ayuda de un return 
+            
+        """
+        
+        return(f"El paciente de sexo {self.sexo} identificado con {self.tipo_doc} el {self.documento} posee una edad de {self.edad} años, un peso de {self.peso} y una estatura de {self.estatura}, cuenta con un tipo de sangre {self.RH}{self.sanguineo}, es alegrico a: {self.alergias}, posee las siguientes comorbilidades: {self.comorbilidades},  el paciente respecto con ser reanimado en caso de que se complique su salud dijo que {self.reanimacion}, ha tenido hasta la fecha {self.embarazos} embarazos y la fecha de su ultima mestruacion fue {self.FUM}, ante su visita al doctor se le hicieron las siguientes observaciones: {self.observaciones} , ademas, requirio una remision {self.remision} ante el especialista de {self.especialista}, el doctor le receto {self.medicacion}. Todo esto a la fecha de {self.actualizacion} ")
+
 
 
 class infoadministrativa:
-    def __init__(self, ticket, historia, documento, profesional, eps, entrada, salida, hospitalizacion, ubicacion, alta):
+    def __init__(self, tipo_doc, documento, nombre, apellido, ticket, historia, documento_prof, profesional, eps, entrada, salida, hospitalizacion, ubicacion, alta):
+        self.tipo_doc=tipo_doc
+        self.documento=documento
+        self.nombre=nombre
+        self.apellido=apellido
         self.ticket=ticket
         self.historia=historia
-        self.documento=documento
+        self.documento_prof=documento_prof
         self.profesional=profesional
         self.eps=eps
         self.entrada=entrada
@@ -56,19 +87,33 @@ class infoadministrativa:
         self.alta=alta
     
     def __str__(self) -> str:
+        """
+        Esta funcion tiene la caracteristica de relatar la informacion administrativa acerca del paciente. 
+        """
+        
         pass
+        #return(El paciente super().paciente())
+        #el paciente (pepito) (perez) identificado con la (cedula) (numero) fue con un ticket de atencion el dia (entrada) a una (hospitalizacion) a cargo del (profesional) identificado con (numero profesional)
+        #gracias a la entidad de seguros (eps) que cubrio todo.
 
 class infoprofesional:
-    def __init__(self, nombre, tipo_doc, documento, tarjetaprof, especialidad, consultorio):
-        self.nombre=nombre
+    def __init__(self, nombre_prof, apellido_prof, tipo_doc, documento_prof, tarjetaprof, especialidad, consultorio):
+        self.nombre_prof=nombre_prof
+        self.apellido_prof=apellido_prof
         self.tipo_doc=tipo_doc
-        self.documento=documento
+        self.documento_prof=documento_prof
         self.tarjetaprof=tarjetaprof
         self.especialidad=especialidad
         self.conslutorio=consultorio
     
     def __str__(self) -> str:
+        """
+        Esta funcion nos genera un resumen de la informacion del profesional que en cuestion atiende al paciente mediante un return convierte el 
+        resumen de los datos en una variable type string 
+        """
+
         pass
+        #El profesional nombre apellido identificado con la tipo_doc documento se identifica con la tarjeta profesional (tarjetaprof) especializado en, ubicado en el consultorio 
 
 
 
@@ -88,8 +133,27 @@ class infofarmaceutica:
     
     
     def __str__(self) -> str:
-        pass
+        """
+        Esta funcion tiene el oficio de resumir el proceso donde un farmaceutico le entrega a un 
+        paciente el medicamento que le preescribio el medico anteriormente recetado donde se especifica el medicamento, la dosis 
+        y la aplicacion del mismo 
 
+           
+        """
+        pass
+        #return(f"el farmaceutica identificado con (tarjeta profesional)  del laboratorio, le suministro al paciente identificado con el (numero) el medicamento () con codigo (), el medicamento se debe administrar () con una dosis de () ")
+
+
+
+listafamaceutica=[]
+
+listaprofesionales=[]
+
+listainfoadministrativa= []
+
+listainfomedica= []
+
+listapacientes = []
 
 
 
@@ -111,6 +175,11 @@ def agregarinfo_p():
 
     Paciente= infopersonalpaciente(tipo_doc, documento, nombre, apellido, numero_contacto, ciudad_residencia, estado_civil, nacionalidad, numero_emergencia, ultima_actualizacion)
     listapacientes.append(Paciente)
+    df= pd.DataFrame(listapacientes)
+    #df.set_index(documento)
+    df.to_csv(r'C:\Users\DELL\OneDrive\Desktop\Idea#2\listapaciente.txt')
+
+
     #print(Paciente)
 
 
@@ -137,14 +206,21 @@ def agregarinfo_m():
 
     info_medica_paciente=infomedica(tipo_doc,documento, historia, edad, sexo, RH, sanguineo, peso, estatura, reanimacion, alergias, comorbilidades, embarazos, FUM, observaciones, remision, especialista, medicacion, actualizacion)
     listainfomedica.append(info_medica_paciente)
+    df1= pd.DataFrame(listainfomedica)
+    #df.set_index(documento)
+    df1.to_csv(r'C:\Users\DELL\OneDrive\Desktop\Idea#2\listainformacionmedica.txt')
 
 
 
 
 def agregarinfo_a():
+    tipo_doc= input("Ingrese tipo de documento (cc, ti, rc, ce, pp): ")
+    documento= input("Ingrese el documento del paciente: ")
+    nombre = input("Ingrese el nombre del paciente: ")
+    apellido = input("Ingrese el apellido del paciente: ")
     ticket = input("Ingrese el ticket de atención: ")
     historia= input("Ingrese el número de la historia clínica del paciente: ")
-    documento = input("Ingrese el número de documento del medico que atiende: ")
+    documento_prof = input("Ingrese el número de documento del medico que atiende: ")
     profesional = input("Ingrese la identificación profesional del médico que atiende: ")
     eps =input("Ingrese la EPS del paciente: ") 
     entrada =input("Ingrese la fecha/hora de entrada: ")
@@ -153,21 +229,28 @@ def agregarinfo_a():
     ubicacion =input("Ingrese la ubicación en la que fue internado el paciente: ")
     alta =input("Ingrese el tipo de alta que se dio al paciente: ")
 
-    info_administrativa_paciente= infoadministrativa(ticket, historia, documento, profesional, eps, entrada, salida, hospitalizacion, ubicacion, alta)
+    info_administrativa_paciente= infoadministrativa(tipo_doc, documento, nombre, apellido, ticket, historia, documento_prof, profesional, eps, entrada, salida, hospitalizacion, ubicacion, alta)
     listainfoadministrativa.append(info_administrativa_paciente)
+    df2= pd.DataFrame(listainfoadministrativa)
+    #df.set_index(documento)
+    df2.to_csv(r'C:\Users\DELL\OneDrive\Desktop\Idea#2\listainformacionadministrativa.txt')
 
 
 
 def agregarinfo_s():
-    nombre = input("Ingrese el nombre del profesional que atendio al paciente: ")
+    nombre_prof = input("Ingrese el nombre del profesional que atendio al paciente: ")
+    apellido_prof = input("Ingrese el apellido del profesional que atendio al paciente: ")
     tipo_doc = input("Ingrese tipo de documento (cc,ti,rc,ce,pp): ")
-    documento = input("Numero de documento: ")
+    documento_prof = input("Numero de documento: ")
     tarjetaprof = input("Numero de la tarjeta profesional: ")
     especialidad = input("Ingrese la especializacion (en caso de no tener coloque no): ")
     consultorio  = input("Numero de consultorio donde se ubica al profesional: ")
 
-    info_profesional_salud= infoprofesional(nombre, tipo_doc, documento, tarjetaprof, especialidad, consultorio)
+    info_profesional_salud= infoprofesional(nombre_prof,apellido_prof, tipo_doc, documento_prof, tarjetaprof, especialidad, consultorio)
     listaprofesionales.append(info_profesional_salud)
+    df3= pd.DataFrame(listaprofesionales)
+    #df.set_index(documento)
+    df3.to_csv(r'C:\Users\DELL\OneDrive\Desktop\Idea#2\listadatosprofesionales.txt')
 
 
 
@@ -187,28 +270,44 @@ def agregarinfo_f():
 
     info_farmaceutica= infofarmaceutica(ticket, documento, historia, farmaceutica, profesional, medicamento, laboratorio, codigo, administracion, cantidad, entrega)
     listafamaceutica.append(info_farmaceutica)
+    df4= pd.DataFrame(listafamaceutica)
+    #df.set_index(documento)
+    df4.to_csv(r'C:\Users\DELL\OneDrive\Desktop\Idea#2\listarecetasfarmaceutica.txt')
 
 
 
-
-
-
-
-
-def lista():
+def lista_p():
     print("--Listado de pacientes--")
     for infopersonalpaciente in listapacientes:
         print(infopersonalpaciente)
 
-listafamaceutica=[]
 
-listaprofesionales=[]
+def lista_m():
+    print("--listado sobre la informacion medica del paciente--")
+    for infomedica in listainfomedica:
+        print(infomedica)
 
-listainfoadministrativa= []
 
-listainfomedica= []
+def lista_a():
+    print("--informacion administrativa--")
+    for infoadministrativa in listainfoadministrativa:
+        print(infoadministrativa)
 
-listapacientes = []
+
+def lista_s():
+    print("--listado de profesionales--")
+    for infoprofesional in listaprofesionales:
+        print(infoprofesional)
+
+
+def lista_f():
+    print("--listado recetas medicamentos--")
+    for infofarmaceutica in listafamaceutica:
+        print(infofarmaceutica)
+
+
+
+
 
 informacion = " "
 while( informacion != "x"):
@@ -221,28 +320,32 @@ while( informacion != "x"):
     print("x - salir de la base de datos")
     informacion = input("Ingrese el tipo de información a la que desea acceder: ")
     if( informacion == "x"):
-        print("Cerrando cesión del sistema: ")
-    elif( informacion == "p"):
-        agregarinfo_p()
-        lista()
-        #print("A - Agregar informacion personal de un paciente: ")
-        #print("M - modificar informacion de un paciente")
-        #print("L - lista de pacientes")
-        #opcion=print("Seleccione la opcion que necesita: ")
+        print("Cerrando cesión del sistema ")
+    if( informacion == "p"):
+        #print("a - agregar informacion nueva")
+        #print("m - modificar informacion existente")
+        #opcion = print("selecciona la opcion que necesitas: ")
+        agregarinfo_p ()
+        lista_p()
+        
         #if(opcion == "a"):
             #agregarinfo_p()
+            #lista_p()
         #elif(opcion == "m"):
-            #pass
-        #elif(opcion == "l"):
-            #lista()
+            #pass 
+
+
     elif( informacion == "m"):
         agregarinfo_m()
+        lista_m()
 
     elif(informacion == "a"):
         agregarinfo_a()
+        lista_a()
     
     elif(informacion == "s"):
         agregarinfo_s()
-
+        lista_s()
     elif(informacion == "f"):
         agregarinfo_f()    
+        lista_f()
